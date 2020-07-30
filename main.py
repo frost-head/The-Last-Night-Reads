1e57291dd8ac4ecc2ff147b4f36f09da8f63cc3b
frost-head <letsgameupatngc@gmail.com>
2020-07-30 08:40:09 +0530

    formsted the code

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask , redirect, render_template,url_for
+from flask import Flask, redirect, render_template, url_for
 from datetime import datetime
 
 # CONFIG
@@ -10,29 +10,38 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def ShowHome():
-    return render_template('Home.html', route = ['/', 'home'] )
+    return render_template('Home.html', route=['/', 'home'])
 
 # LOGIN ROUTE
+
+
 @app.route('/login')
 def login():
-    return render_template('login.html', route = ['login'])
+    return render_template('login.html', route=['login'])
 
 # QUSTIONS ROUTE
+
+
 @app.route('/questions')
 def showQuestions():
-    return render_template('questions.html', route = ['questions'])
+    return render_template('questions.html', route=['questions'])
 
 # PROFILE ROUTE
+
+
 @app.route('/profile')
 def showProfile():
-    return render_template("profile.html", route = ['profile'])
+    return render_template("profile.html", route=['profile'])
 
 # REGISTER ROUTE
+
+
 @app.route('/register')
 def register():
-    return render_template("Register.html", route = ['register'])
-    
+    return render_template("Register.html", route=['register'])
+
 # ROUTES ENDED
 
+
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
