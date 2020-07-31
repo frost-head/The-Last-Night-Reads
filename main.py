30b2c24744ffbd155e8274c5df5af945bbd97475
frost-head <letsgameupatngc@gmail.com>
2020-07-31 21:22:38 +0530

    g

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, redirect, render_template, url_for
 from datetime import datetime
 
+
 # CONFIG
 app = Flask(__name__)
 
@@ -8,35 +9,35 @@ app = Flask(__name__)
 # ROUTES
 # HOME ROUTE
 @app.route('/')
-@app.route('/home')
+@app.route('/home/')
 def ShowHome():
     return render_template('Home.html', route=['/', 'home'])
 
 # LOGIN ROUTE
 
 
-@app.route('/login')
+@app.route('/login/')
 def login():
     return render_template('login.html', route=['login'])
 
 # QUSTIONS ROUTE
 
 
-@app.route('/questions')
+@app.route('/questions/')
 def showQuestions():
     return render_template('questions.html', route=['questions'])
 
 # PROFILE ROUTE
 
 
-@app.route('/profile')
+@app.route('/profile/')
 def showProfile():
     return render_template("profile.html", route=['profile'])
 
 # REGISTER ROUTE
 
 
-@app.route('/register')
+@app.route('/register/')
 def register():
     return render_template("Register.html", route=['register'])
 
